8676c93543c32e0594813f5011378b761658e643
JovanWang <1378744968@qq.com>
2019-04-12 11:09:57 +0800

    corrected model

--- machine_learning/svm/svmtest.py
+++ machine_learning/svm/svmtest.py
@@ -30,8 +30,10 @@ def split_train_dict(df_train,df_train_labels):
 
 def recombine(key_a, feature_list_a, key_b, feature_list_b):
     feature_a = np.array(feature_list_a)
+    # labels_a = np.ones(len(feature_list_a))*(1)
     labels_a = np.ones(len(feature_list_a))*(key_a)
     feature_b = np.array(feature_list_b)
+    # labels_b = np.ones(len(feature_list_b))*(-1)
     labels_b = np.ones(len(feature_list_b))*(key_b)
 
     key_train = np.vstack((feature_a, feature_b))
@@ -79,11 +81,11 @@ class SVM(object):
         solution = cvxopt.solvers.qp(P, q, G, h, A, b)
         alafa = np.ravel(solution['x'])
         # print(alafa)
-        sv = alafa>1e-11
+        sv = alafa>1e-7
         # print(sv)
         index = np.arange(row)[sv]
         self.alafa = alafa[sv]
-        print(self.alafa)
+        # print(self.alafa)
         self.sv_x = X[sv]  # sv's data
         self.sv_y = y[sv]  # sv's labels
         print("%d support vectors out of %d points" % (len(self.alafa), row))
@@ -108,14 +110,18 @@ class SVM(object):
         return np.sign(self.project(X))
 
 class TestAndTrain(object):
-    def __init__(self,kernel_type = 'liner',p=3,C=None):
+    def __init__(self,kernel_type = 'liner',p=3,C=None,a=1,b=-1):
+        self.a = a
+        self.b = b
         self.clf = SVM(kernel_type, p, C)
 
     def trainSVM (self,X_train,y_train):
+        y_train = np.where(y_train == self.a,1.0,-1.0)
         self.clf.fit(X_train, y_train)
 
     def predictSVM(self,X_test):
         y_predict = self.clf.predict(X_test)
+        y_predict = np.where(y_predict == 1.0,self.a,self.b)
         # print(y_predict)
         # print(y_test)
         # correct = np.sum(y_predict == y_test)
@@ -126,34 +132,14 @@ class TestAndTrain(object):
 
 if __name__ == "__main__":
 
-    def liner_test():
-        gen = GenData()
-        tt = TestAndTrain()
-        X1, y1, X2, y2 = gen.liner_data()
-        X_train, y_train = tt.split_train(X1, y1, X2, y2)
-        X_test, y_test = tt.split_test(X1, y1, X2, y2)
-        tt.trainSVM(X_train,y_train,X_test,y_test,'liner')
 
-    def soft_test():
-        gen = GenData()
-        tt = TestAndTrain()
-        X1, y1, X2, y2 = gen.gen_lin_separable_overlap_data()
-        X_train, y_train = tt.split_train(X1, y1, X2, y2)
-        X_test, y_test = tt.split_test(X1, y1, X2, y2)
-        tt.trainSVM(X_train,y_train,X_test,y_test,'liner',3,0.5)
-
-
-    def nonliner_test():
-        gen = GenData()
-        tt = TestAndTrain()
-        X1, y1, X2, y2 = gen.gen_non_lin_separable_data()
-        X_train, y_train = tt.split_train(X1, y1, X2, y2)
-        X_test, y_test = tt.split_test(X1, y1, X2, y2)
-        tt.trainSVM(X_train,y_train,X_test,y_test,'gaussion',5)
+    # tt.trainSVM(X_train,y_train,X_test,y_test,'liner')
+    # tt.trainSVM(X_train,y_train,X_test,y_test,'liner',3,0.5)
+    # tt.trainSVM(X_train,y_train,X_test,y_test,'gaussion',5)
 
     X_train,y_train,X_test,y_test = generate_data()
     train_dict = split_train_dict(X_train,y_train)
-    print(train_dict.keys())
+    # print(train_dict.keys())
     SVM_dict = {}
     train_keys = train_dict.keys()
     # 抽取数据1对1训练：
@@ -162,7 +148,9 @@ if __name__ == "__main__":
             if vs_a == vs_b:
                 continue
             key_train, key_train_labels = recombine(vs_a, train_dict[vs_a], vs_b, train_dict[vs_b])
-            tt = TestAndTrain('gaussion',5)
+            # tt.trainSVM(X_train,y_train,X_test,y_test,'liner',3,0.5, a=vs_a, b=vs_b)
+            # tt = TestAndTrain('liner',3,0.5, a=vs_a, b=vs_b)
+            tt = TestAndTrain('gaussion',5, a=vs_a, b=vs_b)
             tt.trainSVM(key_train,key_train_labels)
             SVM_dict[frozenset((vs_a,vs_b))] = tt
 
@@ -172,14 +160,19 @@ if __name__ == "__main__":
     print("开始测试")
     result = np.array([frozenset(range(1,11)) for _ in range(len(X_test))])
     predict_array = np.column_stack((np.array(X_test),result))
+    # tt = SVM_dict[frozenset((4,8))]
+    # print(SVM_dict[frozenset((1,10))].a,SVM_dict[frozenset((2,8))].a)
+    # y_predict = tt.predictSVM(X_test)
+    # y_pred = y_predict
     for vs_a in range(1,11):
         for vs_b in range(10,0,-1):
-            print(vs_a,vs_b)
+            # print(vs_a, vs_b)
+            # print(predict_array[1:10,-1])
             if vs_a == vs_b:
                 continue
             # 提取出含有当前1对1分类的类别元素，并记录位置
             index_array = []
-            for itera,item in enumerate(predict_array[:,-1].tolist()):
+            for itera,item in enumerate(predict_array[:,-1]):
                 if item.issuperset(frozenset((vs_a,vs_b))):
                     index_array.append(itera)
             test_data = predict_array[index_array,:-1]
@@ -187,24 +180,14 @@ if __name__ == "__main__":
             y_predict = tt.predictSVM(test_data)
             # 根据结果减少划分列别的可能性
             for ind, pred in zip(index_array, y_predict):
-                print(pred)
                 old = predict_array[ind][-1]
                 new = set(old)
                 new.remove(vs_a if pred == vs_b else vs_b)
                 predict_array[ind][-1] = frozenset(new)
     print("结果",predict_array[:,-1])
     y_pred = [list(x)[0] for x in predict_array[:,-1]]
-    print(y_pred)
-    print(y_test)
     correct = np.sum(y_pred == y_test)
     print("%d out of %d predictions correct" % (correct, len(y_pred)))
+    print("正确率为：" , correct/len(y_pred))
 
-    # tt.trainSVM(X_train,y_train)
-    # tt.predictSVM(X_test,y_test)
-    # tt.trainSVM(X_train,y_train,X_test,y_test,'liner',3,0.5)
-
-
-    # liner_test()    
-    #asoft_test()
-    #nonliner_test()  
 
